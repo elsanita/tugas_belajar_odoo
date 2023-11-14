cd514602990e63595b21b93dea61b3612e03a64d
bayu <bayu.fajar@wgs-id.com>
2023-11-14 17:54:30 +0700

    Progress tanggal 14 Nov 2023

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Module Appschef",
-
-    'summary': """
+    "name": "Module Appschef",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         This module was created as learning material for creators
     """,
-
-    'author': "Viendev",
-    'website': "https://www.viendev.com",
-
+    "author": "Viendev",
+    "website": "https://www.viendev.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '16.0.0.1',
-
+    "category": "Employees",
+    "version": "16.0.0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
--- models/model_project.py
+++ models/model_project.py
@@ -1,9 +1,24 @@
 from odoo import models, fields, api
 
+
 class appschef_project(models.Model):
-    _name = 'appschef.appschef_project'
-    _description = 'model for projects'
+    _name = "appschef.appschef_project"
+    _description = "model for projects"
 
     name = fields.Char()
-    teknologi = fields.Selection([("py","Python"),('kt', 'Kotlin'),('java', 'Java'), ('dart', 'Dart'), ('goolang', 'Goolang')])
-    tipe_project = fields.Selection([('et', 'Extended Team'),('fb', 'Fixed Bid'), ('dt', 'Dedicated Team')])
+    teknologi = fields.Selection(
+        [
+            ("py", "Python"),
+            ("kt", "Kotlin"),
+            ("java", "Java"),
+            ("dart", "Dart"),
+            ("goolang", "Goolang"),
+        ]
+    )
+    tipe_project = fields.Selection(
+        [
+            ("et", "Extended Team"),
+            ("fb", "Fixed Bid"),
+            ("dt", "Dedicated Team"),
+        ]
+    )
