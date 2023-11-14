3a120cfaebeef31e4780f80227ef600709e1cadc
gerry-agustian <gerry.agustian@appschef.com>
2023-11-14 16:53:29 +0700

    feat: add method to count projects and get manager name

--- tugas_belajar/models/report.py
+++ tugas_belajar/models/report.py
@@ -7,7 +7,7 @@ class tugas_belajar_report(models.Model):
     _name = 'tugas_belajar.tugas_belajar_report'
     _description = 'tugas_belajar.tugas_belajar_report'
 
-    name = fields.Char()
+    name = fields.Char(readonly=True)
     tanggal = fields.Date(required=True)
     karyawan = fields.Many2one('tugas_belajar.tugas_belajar_karyawan', required=True)
     status = fields.Selection([
@@ -25,3 +25,9 @@ class tugas_belajar_report(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('tugas_belajar.tugas_belajar_report') or _('New')
         res = super(tugas_belajar_report, self).create(vals)
         return res
+    
+    def count_projects(self):
+        return len(self.list_report)
+    
+    def get_manager(self):
+        return self.karyawan.manager.name
