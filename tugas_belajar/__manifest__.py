d2bf7fad66062a92bfdbc6f0acc6a8d2e00d7571
gerry-agustian <gerry.agustian@appschef.com>
2023-11-14 11:43:00 +0700

    feat: create report sequence and add to manifest

--- tugas_belajar/__manifest__.py
+++ tugas_belajar/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/sequence.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
