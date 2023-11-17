12e384076cd6896f3e3f72040727eb25268a5263
gerry-agustian <gerry.agustian@appschef.com>
2023-11-17 11:38:07 +0700

    feat: add context and view id params to wizard

--- tugas_belajar/models/rule_komisi_karyawan.py
+++ tugas_belajar/models/rule_komisi_karyawan.py
@@ -12,14 +12,21 @@ class tugas_belajar_rule_komisi_karyawan(models.Model):
     products = fields.One2many('tugas_belajar.tugas_belajar_rule_komisi_karyawan_product', 'rule_komisi_id')
 
     def add_rule(self):
+        view_id = self.env.ref('tugas_belajar.tugas_belajar_rule_komisi_karyawan_wizard').id
+        context = {
+            'rule_komisi_id': self.id,
+        }
+
         return {
             'name': "Wizard Rule Komisi Karyawan",
             'type': 'ir.actions.act_window',
             'view_type': 'form',
             'view_mode': 'form',
             'res_model': 'tugas_belajar.tugas_belajar_rule_komisi_karyawan_wizard',
-            'view_id': self.env.ref('tugas_belajar.tugas_belajar_rule_komisi_karyawan_wizard').id,
-            'target': 'new'
+            'views': [(view_id, 'form')],
+            'view_id': view_id,
+            'target': 'new',
+            'context': context,
         }
     
 class tugas_belajar_rule_komisi_karyawan_product(models.Model):
@@ -27,5 +34,5 @@ class tugas_belajar_rule_komisi_karyawan_product(models.Model):
     _description = 'tugas_belajar.tugas_belajar_rule_komisi_karyawan_product'
 
     rule_komisi_id = fields.Many2one('tugas_belajar.tugas_belajar_rule_komisi_karyawan', readonly=True)
-    product = fields.Many2one('product.template')
-    persentase = fields.Float()
+    product = fields.Many2one('product.template', readonly=True)
+    persentase = fields.Float(readonly=True)
