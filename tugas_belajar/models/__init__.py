6a09e35494f549b2e6377cd06274ce9007babb65
gerry-agustian <gerry.agustian@appschef.com>
2023-11-20 15:05:53 +0700

    chore: add history komisi karyawan init manifest and security

--- tugas_belajar/models/__init__.py
+++ tugas_belajar/models/__init__.py
@@ -3,6 +3,7 @@
 from . import project
 from . import karyawan
 from . import rule_komisi_karyawan
+from . import history_komisi_karyawan
 from . import report
 from . import project_report
 from . import sale_inherit
