87d0ded484af40680dce4d91df650ee244d50e1c
gerry-agustian <gerry.agustian@appschef.com>
2023-11-20 16:38:25 +0700

    feat: implement get karyawan komisi and extend confirm order method

--- tugas_belajar/models/sale_inherit.py
+++ tugas_belajar/models/sale_inherit.py
@@ -13,7 +13,7 @@ class tugas_belajar_sale_inherit(models.Model):
 
     @api.depends('order_line.price_subtotal', 'order_line.price_tax', 'order_line.price_total')
     def _compute_commisions(self):
-        rule_komisi_karyawan_product_records = self.env['tugas_belajar.tugas_belajar_rule_komisi_karyawan_product'].search([])
+        rule_komisi_karyawan_product_records = self.env['tugas_belajar.tugas_belajar_rule_komisi_karyawan_product'].search([('rule_komisi_id', '=', self.penjual.komisi.id)])
         
         order_lines = self.order_line
 
@@ -24,13 +24,13 @@ class tugas_belajar_sale_inherit(models.Model):
                 if komisi_product.product.id == product.product_template_id.id:
                     self.jumlah_komisi = self.jumlah_komisi + (product.price_unit * komisi_product.persentase/100)
 
-    def write(self, vals):
-        res = super(tugas_belajar_sale_inherit, self).write(vals)
+    def action_confirm(self):
+        res = super(tugas_belajar_sale_inherit, self).action_confirm()
         self.env['tugas_belajar.tugas_belajar_history_komisi_karyawan'].create({
             'so': self.name,
             'karyawan': self.penjual.name,
             'jumlah_komisi': self.jumlah_komisi,
-            'date': self.create_date
+            'date': self.date_order
         })
 
         return res
