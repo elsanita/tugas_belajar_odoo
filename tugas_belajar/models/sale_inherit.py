b1f68186ddcd459e56971cb8d739f33035449410
gerry-agustian <gerry.agustian@appschef.com>
2023-11-20 11:33:41 +0700

    feat: modify compute commisions method to count comissions

--- tugas_belajar/models/sale_inherit.py
+++ tugas_belajar/models/sale_inherit.py
@@ -6,15 +6,20 @@ from odoo import models, fields, api
 class tugas_belajar_sale_inherit(models.Model):
     _inherit = 'sale.order'
     
-    karyawan = fields.Many2one('tugas_belajar.tugas_belajar_karyawan', required=True)
+    penjual = fields.Many2one('tugas_belajar.tugas_belajar_karyawan', required=True)
 
-    total_komisi = fields.Monetary(string="Total Komisi", store=True, compute='_compute_commisions', tracking=4)
+    jumlah_komisi = fields.Monetary(string="Jumlah Komisi", store=True, compute='_compute_commisions', tracking=4, default=0.0)
 
 
     @api.depends('order_line.price_subtotal', 'order_line.price_tax', 'order_line.price_total')
     def _compute_commisions(self):
-        """Compute the total amounts of the SO."""
-        for order in self:
-            order_lines = order.order_line.filtered(lambda x: not x.display_type)
-            
-            order.total_komisi = (order.amount_total * 20/100) #TODO: change to multiply with persentase komisi product
+        rule_komisi_karyawan_product_records = self.env['tugas_belajar.tugas_belajar_rule_komisi_karyawan_product'].search([])
+        
+        order_lines = self.order_line
+
+        self.jumlah_komisi = 0.0
+
+        for product in order_lines:
+            for komisi_product in rule_komisi_karyawan_product_records:
+                if komisi_product.product.id == product.product_template_id.id:
+                    self.jumlah_komisi = self.jumlah_komisi + (product.price_unit * komisi_product.persentase/100)
