91eb618452c0293cfae5990f8d20248e65d34bd5
gerry-agustian <gerry.agustian@appschef.com>
2023-11-23 10:55:58 +0700

    feat: implement js function to show cashier pop up

--- tugas_belajar/static/src/js/ProductScreen.js
+++ tugas_belajar/static/src/js/ProductScreen.js
@@ -1,28 +1,33 @@
-/** @odoo-module **/
+odoo.define('tugas_belajar.ProductScreen', function (require) {
+    'use strict';
 
-import ProductScreen from 'point_of_sale.ProductScreen';
-import Registries from 'point_of_sale.Registries';
+    const ProductScreen = require('point_of_sale.ProductScreen');
+    const Registries = require('point_of_sale.Registries');
+    const { useListener } = require("@web/core/utils/hooks");
 
-export const CashierProductScreen = (ProductScreen) =>
-    class extends ProductScreen {
-        setup() {
-            super.setup();
-            // useListener('click-cashier', this.onClickCashier);
-        }
-        
-        async onClickCashier() {
-            // const selectedOrderline = this.env.pos.get_order().get_selected_orderline();
-            // if (!selectedOrderline) return;
+    const CashierButton = (ProductScreen) =>
+        class extends ProductScreen {
+            setup() {
+                super.setup();
+                useListener('click-cashier', this.onClickCashier);
+            }
+            
+            async onClickCashier() {
+                const selectedOrderline = this.env.pos.get_order().get_selected_orderline();
+                if (!selectedOrderline) return;
 
-            // const { confirmed, payload: inputNote } = await this.showPopup('TextAreaPopup', {
-            //     startingValue: selectedOrderline.get_customer_note(),
-            //     title: this.env._t('Add Customer Note'),
-            // });
+                const { confirmed, payload: inputNote } = await this.showPopup('TextAreaPopup', {
+                    startingValue: selectedOrderline.get_customer_note(),
+                    title: this.env._t('Add Cashier Name'),
+                });
 
-            // if (confirmed) {
-            //     selectedOrderline.set_customer_note(inputNote);
-            // }
-        }
-    };
+                if (confirmed) {
+                    selectedOrderline.set_customer_note(inputNote);
+                }
+            }
+        };
 
-Registries.Component.extend(ProductScreen, CashierProductScreen);
+    Registries.Component.extend(ProductScreen, CashierButton);
+
+    return ProductScreen;
+});
