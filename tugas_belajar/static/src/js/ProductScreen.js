3787e691cffcfa32b01483ff7e1308ce484bf855
gerry-agustian <gerry.agustian@appschef.com>
2023-11-23 17:04:24 +0700

    feat: change const to update and comment error method

--- tugas_belajar/static/src/js/ProductScreen.js
+++ tugas_belajar/static/src/js/ProductScreen.js
@@ -5,7 +5,7 @@ odoo.define('tugas_belajar.ProductScreen', function (require) {
     const Registries = require('point_of_sale.Registries');
     const { useListener } = require("@web/core/utils/hooks");
 
-    const CashierButton = (ProductScreen) =>
+    const Update = (ProductScreen) =>
         class extends ProductScreen {
             setup() {
                 super.setup();
@@ -16,18 +16,20 @@ odoo.define('tugas_belajar.ProductScreen', function (require) {
                 const selectedOrderline = this.env.pos.get_order().get_selected_orderline();
                 if (!selectedOrderline) return;
 
-                const { confirmed, payload: inputNote } = await this.showPopup('TextAreaPopup', {
+                const { confirmed, payload: inputCashierName } = await this.showPopup('TextAreaPopup', {
+                    // startingValue: selectedOrderline.get_cashier_name(),
                     startingValue: selectedOrderline.get_customer_note(),
                     title: this.env._t('Add Cashier Name'),
                 });
 
                 if (confirmed) {
-                    selectedOrderline.set_customer_note(inputNote);
+                    // selectedOrderline.set_cashier_name(inputCashierName);
+                    selectedOrderline.set_customer_note(inputCashierName);
                 }
             }
         };
 
-    Registries.Component.extend(ProductScreen, CashierButton);
+    Registries.Component.extend(ProductScreen, Update);
 
     return ProductScreen;
 });
