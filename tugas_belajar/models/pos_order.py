81668f7a73b151b296e34722ebbf33b79eaf7de1
Bayu <77484284+bayufn22@users.noreply.github.com>
2023-12-05 17:22:13 +0700

    Delete tugas_belajar directory

--- tugas_belajar/models/pos_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class PosOrderLine(models.Model):
-    _inherit = "pos.order.line"
-
-    cashier_name = fields.Char('Cashier Name')
--- tugas_belajar/wizards/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import rule_komisi_karyawan_wizard
